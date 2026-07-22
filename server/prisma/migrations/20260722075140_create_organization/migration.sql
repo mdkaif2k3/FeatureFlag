d1157ff0a964fee9e16dc18119ba13a3111748ab
Mohammed Kaif <mohamkaif2003@gmail.com>
2026-07-22 13:26:18 +0530

    Project Setup

---
+++ server/prisma/migrations/20260722075140_create_organization/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Organization" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_name_key" ON "Organization"("name");
